32c5726c47aad4159acbe3fd01aadc76456e7222
matthew <matthew.villegas@knights.ucf.edu>
2018-10-13 14:16:45 -0400

    Creation of student table

--- dbsetup.js
+++ dbsetup.js
@@ -6,3 +6,6 @@ const connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/
 const client = new pg.Client(connectionString);
 client.connect();
 
+client.query('CREATE TABLE Student (name VARCHAR(20), uid SERIAL PRIMARY KEY, email VARCHAR(80), password VARCHAR(100), university VARCHAR(60))')
+.then(res => console.log("created table!"))
+.catch(e => console.log("Error! ", e.stack))

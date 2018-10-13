b272b0cbdcb2daf5b9536728f0ee05968bd028de
bamurtaugh <bamurtaugh@yahoo.com>
2018-10-13 15:42:38 -0400

    Initial link routing

--- databases-front/src/app/app.module.ts
+++ databases-front/src/app/app.module.ts
@@ -1,16 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RouterModule, Routes } from '@angular/router';
+import { TestCompComponent } from './test-comp/test-comp.component';
+
+const appRoutes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'test', component: TestCompComponent}
+];
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    RouterModule.forRoot(
+     appRoutes,
+      { enableTracing: true }
+    )
   ],
-  providers: [],
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    TestCompComponent
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }

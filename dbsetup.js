99cca36088557ddedecc1dfa9d3b1377e5f328fa
matthew <matthew.villegas@knights.ucf.edu>
2018-10-18 13:22:09 -0400

    added query for creating rso_events

--- dbsetup.js
+++ dbsetup.js
@@ -26,3 +26,8 @@ client.query('CREATE TABLE event (time timestamp, location VARCHAR(300), descrip
 .then(res => console.log("created table!"))
 .catch(e => console.log(e.stack))
 
+client.query('CREATE TABLE rso_event (time timestamp, location VARCHAR(300), description VARCHAR(1000), name VARCHAR(50), contact_email VARCHAR(50), contact_phone VARCHAR(30), event_category VARCHAR(50), rso_id INTEGER, PRIMARY KEY (time, location), FOREIGN KEY (time, location) REFERENCES event(time, location), FOREIGN KEY (rso_id) REFERENCES rso(id))')
+.then(res => console.log("created table!"))
+.catch(e => console.log(e.stack))
+
+

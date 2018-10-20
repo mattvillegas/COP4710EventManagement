e0e3e5aa86a6a00df8186f5123ed62ac82fc59e1
matthew <matthew.villegas@knights.ucf.edu>
2018-10-20 13:59:09 -0400

    added code for creating rso table

--- dbsetup.js
+++ dbsetup.js
@@ -26,8 +26,11 @@ client.query('CREATE TABLE event (time timestamp, location VARCHAR(300), descrip
 .then(res => console.log("created table!"))
 .catch(e => console.log(e.stack))
 
-client.query('CREATE TABLE rso_event (time timestamp, location VARCHAR(300), description VARCHAR(1000), name VARCHAR(50), contact_email VARCHAR(50), contact_phone VARCHAR(30), event_category VARCHAR(50), rso_id INTEGER, PRIMARY KEY (time, location), FOREIGN KEY (time, location) REFERENCES event(time, location), FOREIGN KEY (rso_id) REFERENCES rso(id))')
+client.query('CREATE TABLE rso_event (time timestamp, location VARCHAR(300), description VARCHAR(1000), name VARCHAR(50), contact_email VARCHAR(50), contact_phone VARCHAR(30), event_category VARCHAR(50), rso_id INTEGER, PRIMARY KEY (time, location), FOREIGN KEY (time, location) REFERENCES event(time, location), FOREIGN KEY (rso_id) REFERENCES rso(rso_id))')
 .then(res => console.log("created table!"))
 .catch(e => console.log(e.stack))
 
+client.query('CREATE TABLE rso (name VARCHAR(20), rso_id SERIAL PRIMARY KEY, adminid INTEGER, FOREIGN KEY (adminid) REFERENCES admin(uid))')
+.then(res => console.log("created table!"))
+.catch(e => console.log(e.stack))
 

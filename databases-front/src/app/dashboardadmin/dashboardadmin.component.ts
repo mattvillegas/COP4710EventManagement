2886eb0fd35c59bf103165f14f5c832315fb3823
bamurtaugh <bamurtaugh@yahoo.com>
2018-11-03 13:35:14 -0400

    Moved form to create events to admin instead of user

--- databases-front/src/app/dashboardadmin/dashboardadmin.component.ts
+++ databases-front/src/app/dashboardadmin/dashboardadmin.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, Route } from '@angular/router';
+import { AuthenticationService } from '../_services/authentication.service';
 
 @Component({
   selector: 'app-dashboardadmin',
@@ -7,9 +9,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardadminComponent implements OnInit {
 
-  constructor() { }
+  time: String;
+  location: String;
+  description: String;
+  name: String;
+  title: String;
+  email: String;
+  phone: String;
+  category: String;
+  rsoid: String;
+
+  //RSO: timestamp, location, description, name of person, title of event, contact email, contact phone, event category, id of rso
+
+  constructor(private router: Router, public authService: AuthenticationService) { }
 
   ngOnInit() {
   }
+
+  onAddButton(){
+  	const event = { 
+  		time : this.time,
+  		location : this.location,
+  		description : this.description,
+  		name : this.name,
+  		title : this.title,
+  		email : this.email,
+  		phone : this.phone,
+  		category : this.category,
+  		rsoid : this.rsoid
+  	};
+
+  	this.authService.adminCreateEvent(event).subscribe( 
+  	  data => {
+  	    //alert('You are registered as a student and can now log in');
+        this.router.navigate(['/login']); 
+      }, error => {
+        //alert('Couldn't create student account, please try again');
+        this.router.navigate(['/register']);
+      });
+     } 
 
 }
